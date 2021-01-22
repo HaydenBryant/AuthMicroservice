77df0a730394d8b47e9dead115ee159fee9ee9b1
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-01-21 22:11:43 -0500

    bcrypt added

--- src/main/java/com/auth/controller/UserController.java
+++ src/main/java/com/auth/controller/UserController.java
@@ -3,6 +3,7 @@ package com.auth.controller;
 import com.auth.entity.User;
 import com.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +15,11 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @PostMapping("/addUser")
+    private BCryptPasswordEncoder bCrypt;
+
+    @PostMapping("/register")
     public User addUser(@RequestBody User user){
+        user.setPassword(bCrypt.encode(user.getPassword()));
         return service.saveUser(user);
     }
 
--- src/main/java/com/auth/service/UserService.java
+++ src/main/java/com/auth/service/UserService.java
@@ -3,9 +3,12 @@ package com.auth.service;
 import com.auth.entity.User;
 import com.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class UserService {
     @Autowired
     private UserRepository repo;
