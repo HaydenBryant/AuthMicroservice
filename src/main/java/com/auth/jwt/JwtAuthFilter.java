ab1a338396acf0aca0f2fb6dd9ac880fb63e4baf
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-01-22 15:44:50 -0500

    having dependency troubles

--- src/main/java/com/auth/jwt/JwtAuthFilter.java
+++ src/main/java/com/auth/jwt/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.auth.jwt;
 
+import com.auth.jwt.JWT;
 import com.auth.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -39,6 +45,19 @@ public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String token = JWT.create()
+                .withSubject(((User) auth.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwt.token.expiration.in.seconds)
+                .sign(HMAC512(jwt.signing.key.secret.getBytes()));
+        res.addHeader(jwt.http.request.header, "Bearer " + token);
     }
 
 
